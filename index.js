bb9cbaa84784785b9a5a9ccfba00fca9925c4567
Muhammad Ihsanul 'Arifin <ihsan.ihsanularifinm@gmail.com>
2025-08-31 15:01:29 +0700

    feat: Add cors middleware to allow frontend access

--- index.js
+++ index.js
@@ -1,21 +1,21 @@
 const express = require('express');
-const connectDB = require('./config/db'); // Ini sudah benar
+const connectDB = require('./config/db');
+const cors = require('cors'); // 1. Impor cors
 const beritaRoutes = require('./routes/beritaRoutes');
 
-// Hubungkan ke Database
 connectDB();
-
 const app = express();
-const PORT = 5000;
 
-// Middleware untuk menerima body request dalam format JSON
+// 2. Gunakan middleware cors agar frontend bisa mengakses API ini
+app.use(cors());
+
+const PORT = 5000;
 app.use(express.json());
 
 app.get('/', (req, res) => {
 	res.send('API berjalan...');
 });
 
-// Gunakan rute berita
 app.use('/api/berita', beritaRoutes);
 
 app.listen(PORT, () => {
