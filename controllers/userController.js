5e885bb4b891f3efc5d965b98f7813b2ea0e83a0
Muhammad Ihsanul 'Arifin <ihsan.ihsanularifinm@gmail.com>
2025-09-14 21:25:52 +0700

    Update userController.js

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,8 +1,6 @@
 const User = require('../models/userModel');
 const generateToken = require('../utils/generateToken');
 
-// @desc    Mendaftarkan user admin baru
-// @route   POST /api/users/register
 const registerUser = async (req, res) => {
 	try {
 		const { nama, username, password } = req.body;
@@ -11,12 +9,7 @@ const registerUser = async (req, res) => {
 		if (userExists) {
 			return res.status(400).json({ message: 'Username sudah digunakan' });
 		}
-
-		const user = await User.create({
-			nama,
-			username,
-			password, // Password akan di-hash otomatis oleh model
-		});
+		const user = await User.create({ nama, username, password });
 
 		if (user) {
 			res.status(201).json({
@@ -29,18 +22,16 @@ const registerUser = async (req, res) => {
 			res.status(400).json({ message: 'Data user tidak valid' });
 		}
 	} catch (error) {
+		console.error('ERROR saat registerUser:', error);
 		res.status(500).json({ message: 'Server Error' });
 	}
 };
 
-// @desc    Login & mendapatkan token
-// @route   POST /api/users/login
 const loginUser = async (req, res) => {
 	try {
 		const { username, password } = req.body;
 		const user = await User.findOne({ username });
 
-		// Cek apakah user ada DAN password cocok (menggunakan method dari model)
 		if (user && (await user.matchPassword(password))) {
 			res.json({
 				_id: user._id,
@@ -52,6 +43,7 @@ const loginUser = async (req, res) => {
 			res.status(401).json({ message: 'Username atau password salah' });
 		}
 	} catch (error) {
+		console.error('!!! TERJADI ERROR FATAL SAAT LOGIN:', error);
 		res.status(500).json({ message: 'Server Error' });
 	}
 };
